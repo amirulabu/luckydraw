143beb398437ada6c1a807f2ba50a19ba5898e76
amirulabu <amirulabu@gmail.com>
2016-12-31 22:48:09 +0800

    edit css, index controller, generate scaffold Person

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def index
+  	@persons = Person.all.shuffle
+
+  	if user_signed_in?
+  		@lucky_person = Person.all.shuffle.first
+  	end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161231124918) do
+ActiveRecord::Schema.define(version: 20161231132313) do
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.string   "petid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
