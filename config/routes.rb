af688bbc84ee482aa64db7bcc038b0cd378d478a
amirulabu <amirulabu@gmail.com>
2017-01-01 03:14:23 +0800

    remove sign in, config routes, add admin and super admin to users, edit people index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
-  resources :people
-  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
+  get '/list', to: "pages#show"
+
+  
+  authenticate :user do
+  	resources :people, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
+  resources :people, only: [:show]
+
+  devise_for :users, path_names: {sign_up: 'let_me_in'}
+
 
 end
+
+
+## admin - can add person
+
+## super admin - can add admin, can add person
