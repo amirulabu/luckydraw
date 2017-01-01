6377b7042a78778276e2137e207a1e70599c661f
amirulabu <amirulabu@gmail.com>
2017-01-01 10:55:29 +0800

    add pagination in list, add winner to person, trying routes and random using person

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161231184305) do
+ActiveRecord::Schema.define(version: 20161231235029) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
     t.string   "petid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "winner",     default: 0
   end
 
   create_table "users", force: :cascade do |t|
